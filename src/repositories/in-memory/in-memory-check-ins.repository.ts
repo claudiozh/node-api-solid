4d9c3b64f38e996f98f53deedb1cb5b999101e63
claudiozh <claudiorodrigozh@gmail.com>
2023-06-16 08:56:12 -0300

    feat: added validations for the user not to login twice in the same day

--- src/repositories/in-memory/in-memory-check-ins.repository.ts
+++ src/repositories/in-memory/in-memory-check-ins.repository.ts
@@ -1,6 +1,7 @@
 import { CheckIn, Prisma } from '@prisma/client'
 import { CheckInsRepository } from '@/repositories/check-ins.repository'
 import { randomUUID } from 'node:crypto'
+import dayjs from 'dayjs'
 
 export class InMemoryCheckInsRepository implements CheckInsRepository {
   private items: CheckIn[] = []
@@ -10,12 +11,34 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
       id: randomUUID(),
       created_at: new Date(),
       gym_id: data.gym_id,
-      user_id: data.gym_id,
+      user_id: data.user_id,
       validated_at: data.validated_at ? new Date(data.validated_at) : null,
     }
 
     this.items.push(checkIn)
 
     return checkIn
+  }
+
+  async findByUserIdOnDate(
+    userId: string,
+    date: Date,
+  ): Promise<CheckIn | null> {
+    const startOfTheDay = dayjs(date).startOf('date')
+    const endOfTheDay = dayjs(date).endOf('date')
+
+    const checkInOnSameDate = this.items.find((checkIn) => {
+      const checkInDate = dayjs(checkIn.created_at)
+      const isOnSameDate =
+        checkInDate.isAfter(startOfTheDay) && checkInDate.isBefore(endOfTheDay)
+
+      return checkIn.user_id === userId && isOnSameDate
+    })
+
+    if (!checkInOnSameDate) {
+      return null
+    }
+
+    return checkInOnSameDate
   }
 }
