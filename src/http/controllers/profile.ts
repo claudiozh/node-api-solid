6c85a3d4da5f5a684fb23f56cc7a4f39b8ae3fb1
claudiozh <claudiorodrigozh@gmail.com>
2023-06-29 08:37:46 -0300

    feat: add controller get profile

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -6,14 +6,14 @@ export async function profile(request: FastifyRequest, reply: FastifyReply) {
 
   const getUserProfileUseCase = makeGetUserProfileUseCase()
 
-  const user = await getUserProfileUseCase.execute({
+  const { user } = await getUserProfileUseCase.execute({
     userId: request.user.sub,
   })
 
   return reply.status(200).send({
     user: {
       ...user,
-      password_hash: 'sdfsdfs',
+      password_hash: undefined,
     },
   })
 }
